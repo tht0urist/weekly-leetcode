5c607df1b7740410ee588e1d29cc16ad5a70fa04
Oualid <bouh.oualid@gmail.com>
2023-09-08 15:49:11 +0200

    Create RemoveDuplicatesFromSortedArray.java

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set numsSet = new TreeSet<Integer>(new Comparator<Integer>() {
+            public int compare(Integer number1, Integer number2) {
+                return number1 - number2;
+            }
+        });
+        for(int i =0 ; i < nums.length ; i++){
+            numsSet.add(nums[i]);
+        }
+        int index = 0;
+        Iterator<Integer> numsIterator = numsSet.iterator();
+        while(numsIterator.hasNext()) {
+            nums[index] = numsIterator.next();
+            index++;
+        }
+        return numsSet.size();
+    }
+}
